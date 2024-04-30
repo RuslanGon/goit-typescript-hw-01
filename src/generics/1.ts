f92bfcf5c230cb36dfb96701460c911c0bf2e0e5
RuslanGon <ruslan441006@gmail.com>
2024-04-30 13:01:09 +0300

    wer

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-async function fetchData(url: string): Promise<any> {
+async function fetchData(url: string): Promise<string> {
   try {
     const response = await axios.get(url);
     return response.data;
